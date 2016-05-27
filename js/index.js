2e242627d6708a42d386c23a7017c4fb648e4b87
Anoushka Ramkumar <ankyisthebest@gmail.com>
2016-05-27 17:12:43 -0400

    ADDING A BUNCH OF FILES

---
+++ js/index.js
@@ -0,0 +1,74 @@
+$( document ).ready(function() {
+
+	$( ".AAST" ).hover(
+	  function() {
+	    $( ".AAST-i" ).css("display", "inline-block !important");
+	  }, function() {
+	    $( ".AAST-i" ).css("display", "none !important");
+	  }
+	);
+
+	$( ".ATCS" ).hover(
+	  function() {
+	    $( ".ATCS-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".ATCS-i" ).css("display", "inline-block !important");
+	  }
+	);
+
+	$( ".AMST" ).hover(
+	  function() {
+	    $( ".AMST-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".AMST-i" ).css("display", "inline-block !important");
+	  }
+	);
+
+	$( ".AEDT" ).hover(
+	  function() {
+	    $( ".AEDT-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".AEDT-i" ).css("display", "inline-block !important");
+	  }
+	);
+
+	$( ".ACHA" ).hover(
+	  function() {
+	    $( ".ACHA-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".ACHA-i" ).css("display", "inline-block !important");
+	  }
+	);
+
+	$( ".ABF" ).hover(
+	  function() {
+	    $( ".ABF-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".ABF-i" ).css("display", "inline-block !important");
+	  }
+	);
+
+	$( ".AVPA-M" ).hover(
+	  function() {
+	    $( ".AVPA-M-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".AVPA-M-i" ).css("display", "inline-block !important");
+	  }
+	);
+
+	$( ".AVPA-V" ).hover(
+	  function() {
+	    $( ".AVPA-V-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".AVPA-V-i" ).css("display", "inline-block !important");
+	  }
+	);
+
+	$( ".AVPA-T" ).hover(
+	  function() {
+	    $( ".AVPA-T-i" ).css("display", "none !important");
+	  }, function() {
+	    $( ".AVPA-T-i" ).css("display", "inline-block !important");
+	  }
+	);
+});
